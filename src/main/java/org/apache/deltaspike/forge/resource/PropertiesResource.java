b8c09c4cae67fc7d930b5923ceed672d2394beea
Rudy De Busscher <rdebusscher@gmail.com>
2012-02-12 21:10:30 +0100

    Support for properties and service files

---
+++ src/main/java/org/apache/deltaspike/forge/resource/PropertiesResource.java
@@ -0,0 +1,88 @@
+package org.apache.deltaspike.forge.resource;
+
+import org.jboss.forge.project.services.ResourceFactory;
+import org.jboss.forge.resources.FileResource;
+import org.jboss.forge.resources.Resource;
+import org.jboss.forge.resources.ResourceHandles;
+
+import javax.inject.Inject;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ */
+@ResourceHandles(value = "*.properties")
+public class PropertiesResource extends FileResource<PropertiesResource> {
+
+    private Properties properties;
+    
+    @Inject
+    public PropertiesResource(final ResourceFactory factory)
+    {
+        super(factory, null);
+    }
+
+    public PropertiesResource(ResourceFactory factory, File file) {
+        super(factory, file);
+        loadProperties(file); 
+    }
+
+    private void loadProperties(File someFile) {
+        if (someFile.exists()) {
+            properties = new Properties();
+            try {
+                FileInputStream stream = new FileInputStream(someFile);
+                properties.load(stream);
+                stream.close();
+            } catch (IOException e) {
+                // FIXME
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean containsKey(String keyValue) {
+        return properties.keySet().contains(keyValue);
+    }
+    
+    public Set<String> keyValues() {
+        // Don't want to use 1.6 functionality here (yet)
+        Set<String> result = new HashSet<String>();
+        Enumeration<?> enumeration = properties.propertyNames();
+        while (enumeration.hasMoreElements()) {
+            result.add((String) enumeration.nextElement());
+        }
+        return result;
+    }
+    
+    public String getValue(String key) {
+        return properties.getProperty(key);
+    }
+    
+    public void setValue(String key, String value) {
+        properties.setProperty(key, value);
+        storeProperties();
+    }
+
+    private void storeProperties() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            properties.store(stream, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setContents(stream.toString());
+
+    }
+
+    @Override
+    public PropertiesResource createFrom(File file) {
+        return new PropertiesResource(resourceFactory, file);
+    }
+
+    @Override
+    public List<Resource<?>> listResources() {
+       return Collections.emptyList();
+    }
+}
